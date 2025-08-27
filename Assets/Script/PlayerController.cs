c0ca5d52ed9d75e609e958fdc28231a3e1773255
Andres Felipe Patarroyo Muñoz <159494183+AndresPatarroyo1517@users.noreply.github.com>
2025-08-26 20:20:02 -0500

    Cosas varias

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,18 +1,30 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
-
+using TMPro;
 public class PlayerController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private int contador;
     private Rigidbody rb;
     public float speed;
     public float jumpForce = 5f;
     private bool isGrounded = true;
+    public TextMeshProUGUI puntajeGUI;
+    public GameObject mensajeGanar;
+
+    public Transform particles;
+    private ParticleSystem particleSystem;
+    private Vector3 position;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        contador = 0;
+        SetCountText();
+        mensajeGanar.SetActive(false);
+        particleSystem = particles.GetComponent<ParticleSystem>();
+        particleSystem.Stop();
     }
 
     // Update is called once per frame
@@ -30,14 +42,33 @@ public class PlayerController : MonoBehaviour
         float horizontalMove = Input.GetAxis("Horizontal");
         float verticalMove = Input.GetAxis("Vertical");
         Vector3 move = new Vector3(horizontalMove, 0.0f, verticalMove);
-        rb.AddForce(move*speed);
+        rb.AddForce(move * speed);
     }
-    
-     private void OnCollisionEnter(Collision collision)
+
+    void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Ground")) 
+        if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Recolectable"))
+        {
+            other.gameObject.SetActive(false);
+            contador += 1;
+            SetCountText();
+        }
+    }
+    
+    void SetCountText()
+    {
+        puntajeGUI.text = "Puntaje: " + contador.ToString();
+        if (contador >= 30)
+        {
+            mensajeGanar.SetActive(true);
         }
     }
 }
